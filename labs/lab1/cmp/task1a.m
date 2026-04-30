31ed18202f20f24223fc5d3af213dd7301fa7e54
Ilya Semenov <hi@si1og.ru>
2026-04-30 15:27:28 +0300

    [lab1]: add todo macors and todss to lab1

--- labs/lab1/cmp/task1a.m
+++ labs/lab1/cmp/task1a.m
@@ -1,19 +1,24 @@
-x = [1.41, 2.31, 4.13, 5.31, 6.01];
-y = [-1.4156, 2.3901, 3.0567, 0.9812, 2.7569];
+pkg load symbolic
+
+xd = [1.41, 2.31, 4.13, 5.31, 6.01];
+yd = [-1.4156, 2.3901, 3.0567, 0.9812, 2.7569];
 
 x_interp = 4.96;
 L = 0;
 
-n = length(x);
+syms x
+
+n = length(xd);
 for i = 1:n
     L_i = 1;
     for j = 1:n;
         if i ~= j
-            L_i = L_i * (x_interp - x(j)) / (x(i) - x(j))
+            L_i = L_i * (x - xd(j)) / (xd(i) - xd(j))
         end
     end
-    L = L + y(i) * L_i
+    L = L + yd(i) * L_i
+    latex(L)
 end
 
-
-fprintf('%f\n', L)
+latex_formula = latex(L)
+disp(latex_formula)
